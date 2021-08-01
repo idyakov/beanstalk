d4924e535c486c257e313cb89a89e184e9be439a
Ilya Dyakov <dyak.ilya@gmail.com>
2021-08-01 14:45:13 -0700

    v.2

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ else
         <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
         <p>This environment is launched with Elastic Beanstalk PHP Platform</p>
-		<p>This code has been  deployed from GitHub</p>
+		<p>This code has been  deployed from GitHub via Jenkins</p>
     </section>
 
     <section class="instructions">
